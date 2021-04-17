00dc74d96392e118ab0df4a11a71b54c651ca48d
Stefan Löwe <stefan@loewe.ws>
2021-04-17 20:54:11 +0200

    extend example by reading out also a value

--- src/demo.php
+++ src/demo.php
@@ -14,6 +14,10 @@ $logger->pushHandler(new StreamHandler(__DIR__.'/../log.log', Logger::INFO));
 $connector = new PolyphaseMeterConnector('/dev/ttyUSB0');
 
 $reading = $connector->read();
-
 echo 'raw data: ' . $reading . PHP_EOL;
 $logger->info('raw data: ' . $reading . PHP_EOL);
+
+$currentWatts = (double) $reading->extract('1-0:16.7.0*255')[1];
+echo 'raw current watts: ' . $currentWatts . PHP_EOL;
+$logger->info('raw data: ' . $currentWatts . PHP_EOL);
+
