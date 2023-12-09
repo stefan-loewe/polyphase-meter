6c2761ee84747aae2ed3f38a0b5f94486938b5e7
Mario Linker <15095261+maldag@users.noreply.github.com>
2023-12-09 09:49:58 +0100

    Zyklisches Abfragen des Smart Meters sowie Ausgabe in CSV Datei

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 stty -F /dev/ttyUSB0 1:0:8bd:0:3:1c:7f:15:4:5:1:0:11:13:1a:0:12:f:17:16:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0:0 2>/dev/null
 
-php src/demo.php
+while true; do
+    php src/demo.php
+    sleep 60
+done
--- src/demo.php
+++ src/demo.php
@@ -7,17 +7,33 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
 
-$logger = new Logger('polyphase_meter');
-$logger->pushHandler(new StreamHandler(__DIR__.'/../log.log', Logger::INFO));
+$logger_raw = new Logger('polyphase_meter');
+$logger_raw->pushHandler(new StreamHandler(__DIR__.'/../log_raw.log', Logger::INFO));
+
+$logger_csv = new Logger('polyphase_meter');
+$stream = new StreamHandler(__DIR__.'/../data.csv', Logger::INFO);
+
+$dateFormat = "Y-m-d\TH:i:s";
+$output = "%datetime%;%message%\n";
+
+// finally, create a formatter
+$formatter = new LineFormatter($output, $dateFormat);
+$stream->setFormatter($formatter);
+$logger_csv->pushHandler($stream);
+
 
 $connector = new PolyphaseMeterConnector('/dev/ttyUSB0');
 
 $reading = $connector->read();
-echo 'raw data: ' . $reading . PHP_EOL;
-$logger->info('raw data: ' . $reading . PHP_EOL);
+#echo 'raw data: ' . $reading . PHP_EOL;
+$logger_raw->info('raw data: ' . $reading . PHP_EOL);
+
 
 $currentWatts = (double) $reading->extract('1-0:16.7.0*255')[1];
-echo 'raw current watts: ' . $currentWatts . PHP_EOL;
-$logger->info('raw data: ' . $currentWatts . PHP_EOL);
+$totalEnergy = (double) $reading->extract('1-0:1.8.0*255')[1];
+
+#echo 'raw current watts: ' . $currentWatts . PHP_EOL;
+$logger_csv->info($currentWatts . ";" . $totalEnergy );
 
